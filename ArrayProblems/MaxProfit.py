9acc7731a372068b41b21dd9f6dbe2e027a9e934
07prajwal2000 <manuaradhya07@gmail.com>
2022-03-12 19:10:22 +0530

    Moved to Folder

---
+++ ArrayProblems/MaxProfit.py
@@ -0,0 +1,18 @@
+from typing import List
+
+array: List[int] = [7, 3, 5, 3, 6, 8]
+
+def CalculateProfit(prices: List[int]):
+    left, right = 0, 1
+    max_profit = 0
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            max_profit = max(profit, max_profit)
+        else:
+            left = right
+        right += 1
+    
+    return max_profit
+
+print(CalculateProfit(array))
